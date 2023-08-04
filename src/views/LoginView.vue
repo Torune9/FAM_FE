35514b638ea5290a858d2cac79499e72589fe607
achmadi ikhsan <achmadiikhsan01@gmail.com>
2023-08-04 16:23:54 +0700

    login layout

---
+++ src/views/LoginView.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container">
+        <div class="flex justify-center items-center  w-full flex-wrap ">
+            <form action="" class="flex flex-wrap w-10 justify-center items-center">
+            <label class="mt-auto">login</label>
+            <label for="username">username</label>
+            <input type="text" name="username" id="username">
+            <label for="password">password</label>
+            <input type="text" name="password" id="password">
+            <button class="bg-sky-700 my-3">login</button>
+            <p>forgot</p>
+            </form>
+        </div>
+    </div>
+</template>
