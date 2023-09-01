574dbcee594755dbc11cb2339c174b915fa7c081
rahman <rahmannurhayadi@gmail.com>
2023-09-01 14:28:10 +0700

    fix:improve layout login

--- src/components/LoginView.vue
+++ src/components/LoginView.vue
@@ -1,34 +1,29 @@
 <template>
-<div class="flex justify-center items-center  w-screen h-screen bg-white">
-    <div class="h-[400px] w-[400px] border-2 border-indigo-400 rounded-sm flex flex-wrap justify-center items-center ">
-        <p class="text-center text-black w-full">Login</p>
+<div class="flex justify-center items-center  w-screen h-screen">
+    <div class="h-[400px] w-[400px] bg-slate-800 rounded-md flex flex-wrap justify-center items-center text-zinc-100 shadow-xl">
+        <p class="text-center w-full font-extrabold text-2xl">Login Form</p>
         <form action="" class="grid grid-rows-3 h-40 gap-10 ">
-        <div class="text-black">
-            <label for="username">username</label>
+        <div>
+            <label class="font-bold" for="username">username</label>
             <br>
-            <input type="text" name="username" id="username" class="h-8 w-[180px] rounded-sm border border-indigo-500 outline-none">
+            <input type="text" name="username" id="username" class="h-8 w-[250px] text-center text-sm border-2 rounded-md border-slate-200 bg-transparent outline-none" required>
         </div>
-        <div class="text-black">
-            <label for="password">password</label>
+        <div>
+            <label class="font-bold" for="password">password</label>
             <br>
-            <input type="text" name="password" id="password" class="w-[180px] h-8 rounded-sm border border-indigo-500 outline-none">
+            <input type="password" name="password" id="password" class="w-[250px] text-center text-sm border-2 h-8 rounded-md border-slate-200 bg-transparent outline-none" required>
         </div>
         <div class="flex justify-center items-center">      
-            <button class="bg-indigo-600 w-24 h-9 mt-20 rounded-md text-white font-semibold" @click="gotoDasboard">Login</button>
+            <button class=" bg-blue-600 w-[250px] h-9 mt-20 rounded-md text-white font-semibold">Login</button>
         </div>
         </form>
         <!-- login -->
-        <p class=" w-full text-center text-sm"><a href="" class="text-indigo-500">forgot password ?</a></p>
+        <p class=" w-full text-center text-sm"><router-link to ="/forgot-password">Forgot password?</router-link></p>
     </div>
 </div>
 </template>
 
 <script setup>
-import { useRouter } from 'vue-router'
-
-const router = useRouter()
-
-const gotoDasboard = () => {
-    router.push({ name:'Dashboard' })
-}
 </script>
+<style>
+</style>
