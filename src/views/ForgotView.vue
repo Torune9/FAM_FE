8abd3a7f09e3b40732715e187e3e30e6d4a43417
rahman <rahmannurhayadi@gmail.com>
2023-09-06 16:20:41 +0700

    feat:adding login button to redirect

--- src/views/ForgotView.vue
+++ src/views/ForgotView.vue
@@ -4,7 +4,7 @@
 <template>
   <main>
         <div class=" w-screen h-screen flex justify-center items-center bg-slate-300">
-           <div class="rounded-md flex justify-center items-center flex-wrap h-[400px] w-[400px] p-4 shadow-lg bg-slate-800 text-zinc-200">
+           <div class="rounded-md flex justify-center items-center flex-wrap h-[400px] w-[400px] p-4 shadow-lg bg-slate-800 text-zinc-200 relative">
             <h1 class=" text-2xl font-semibold mb-9">Forgot password</h1>
             <small class=" mb-4">Please provide your email addres that you used when you signed up for your account.</small>
             <form action="#" method="post" class="flex flex-wrap gap-5">
@@ -14,13 +14,17 @@
                     <input type="text" id = "email" class=" w-60 h-9 outline-none border border-slate-600 rounded-md text-center text-sm bg-transparent" required placeholder="email">
                 </div>
                 <small>We will send you and email that will allow you to reset password.</small>
-                <div><Button class=" bg-yellow-500 w-40  rounded-md h-10 font-semibold text-sm text-zinc-100">Reset password</Button></div>
-            </form>
+                <div><Button class=" bg-yellow-500 w-40  rounded-md h-10 font-semibold text-sm text-zinc-100">Reset password</Button>
+                </div>
+              </form>
+              <div>
+                <button class=" bg-blue-600 rounded-tl-md w-16 text-sm absolute bottom-0 right-0"><router-link to="/">Login</router-link></button>
+              </div>
            </div>
         </div>
   </main>
 </template>
-<style>
+<style> 
  div{
   font-family: 'Poppins', sans-serif;
   }
