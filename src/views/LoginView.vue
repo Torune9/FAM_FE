efb91ecb9e29a233ff973e3318c2dd7ebaf7bd2d
rahman <rahmannurhayadi@gmail.com>
2023-09-06 16:24:14 +0700

    feat:create post request to api

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,21 +1,20 @@
-<script >
-</script>
-
 <template>
   <main>
     <div class="flex justify-center items-center  w-screen h-screen">
     <div class="h-[400px] w-[400px] bg-slate-800 rounded-md flex flex-wrap justify-center items-center text-zinc-100 shadow-xl">
+        <div class=" w-[20%]  mt-6 rounded-md bg-red-300 flex justify-center items-center text-red-600 h-8 absolute top-4" v-if="auth.eror"><small class=" font-bold">{{ auth.eror }}</small></div>
         <p class="text-center w-full font-extrabold text-2xl">Login Form</p>
-        <form action="#" method="post" class="grid grid-rows-3 h-40 gap-10 ">
+        <form @submit.prevent="login" class="grid grid-rows-4 h-44 gap-10 justify-items-center">
         <div>
             <label class="font-bold" for="username">username</label>
             <br>
-            <input type="text" name="username" id="username" class="h-8 w-[250px] text-center text-sm border-2 rounded-md border-slate-200 bg-transparent outline-none" required>
+            <input type="text" name="username" id="username" class="h-8 w-[250px] text-center text-sm border-2 rounded-md border-slate-200 bg-transparent outline-none" v-model="username" >
         </div>
+
         <div>
             <label class="font-bold" for="password">password</label>
             <br>
-            <input type="password" name="password" id="password" class="w-[250px] text-center text-sm border-2 h-8 rounded-md border-slate-200 bg-transparent outline-none" required>
+            <input type="password" name="password" id="password" class="w-[250px] text-center text-sm border-2 h-8 rounded-md border-slate-200 bg-transparent outline-none" v-model="password" >
         </div>
         <div class="flex justify-center items-center">      
             <button class=" bg-blue-600 w-[250px] h-9 mt-20 rounded-md text-white font-semibold">Login</button>
@@ -26,8 +25,43 @@
     </div>
 </div>
   </main>
-  <router-view/>
+  <router-view/>)
 </template>
+
+<script>
+    import {useVuelidate} from '@vuelidate/core';
+    import { required } from '@vuelidate/validators';
+    import {loginStore} from '../store/loginStore'
+
+    export default{
+        setup(){
+            const auth = loginStore()
+            return{
+                v$ : useVuelidate(),
+                auth,
+            }
+        },
+        data(){
+            return{
+                username : '',
+                password : '',
+            }
+        },
+        validations(){
+            return {
+              username : {required},
+              password : {required}
+            }
+        },
+        methods:{
+          login(){
+            this.v$.$validate()
+            this.auth.signIn(this.username,this.password)
+          }
+        },
+    }
+    
+</script>
 
 <style>
 </style>
