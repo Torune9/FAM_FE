60c21524cab3d00e29f848e31b6a627e446368f6
rahman <rahmannurhayadi@gmail.com>
2023-09-06 16:23:33 +0700

    feat:add dependency

--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+const pinia  = createPinia()
+
+app.use(pinia)
 
 app.use(router)
 
