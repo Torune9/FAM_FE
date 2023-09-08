807be68d79d9440c632750fe51cd725f0803c5b0
rahman <rahmannurhayadi@gmail.com>
2023-09-08 11:13:38 +0700

    delete apiStore

--- src/store/apiStore.js
+++
@@ -1,9 +0,0 @@
-import { defineStore } from "pinia";
-
-const apiStore = defineStore('api',{
-    state:()=>({
-        API_URL : 'http://localhost:3000/api/'
-    }),
-})
-
-export default apiStore
