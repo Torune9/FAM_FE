375a170039fbd11d56c521221c02172fe281d067
Tri Sutrisno <tri.s@bts.id>
2023-09-14 09:40:03 +0700

    fix sidebar use const for dynamic menu

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,23 +1,36 @@
 <template>
     <aside>
         <nav class=" bg-redHunt text-zinc-100 w-[20%] p-4 h-screen">
-       <div class=" grid grid-rows-2 gap-3 ">
-        <div class="">
-            <img src="https://source.unsplash.com/600x400" alt="" >
-            <br>
-        <hr>
-        <hr>
-        </div>
-        <ul class="grid grid-rows-3 font-semibold text-sm">
-            <li class="h-10 flex items-center p-2 before:contents[''] before:bg-red-300 before:h-10 before:w-1 before:mr-3 active:text-cyan-400 before:scale-y-0 hover:before:scale-y-100 before:transition-transform hover:text-red-100 active:scale-y-100"><router-link to="/dashboard">Dashboard</router-link></li>
-            <li class="h-10 flex items-center p-2 before:contents[''] before:bg-red-300 before:h-10 before:w-1 before:mr-3 active:text-cyan-400 before:scale-y-0 hover:before:scale-y-100 before:transition-transform hover:text-red-100"><router-link to="#">Master Data Asset</router-link></li>
-            <li class="h-10 flex items-center p-2 before:contents[''] before:bg-red-300 before:h-10 before:w-1 before:mr-3 active:text-cyan-400 before:scale-y-0 hover:before:scale-y-100 before:transition-transform hover:text-red-100"><router-link to="#">Asset</router-link></li>
-        </ul>
-       </div>
-    </nav>
+            <div class=" grid grid-rows-2 gap-3 ">
+                <div class="">
+                    <!-- <img src="https://source.unsplash.com/600x400" alt="" > -->
+                    <br>
+                    <hr>
+                    <hr>
+                </div>
+                <ul class="grid grid-rows-3 font-semibold text-sm">
+                    <li v-for="menu, index in menus" :key="index"
+                        class="h-10 flex items-center p-2 before:contents[''] before:bg-red-300 before:h-10 before:w-1 before:mr-3 active:text-cyan-400 before:scale-y-0 hover:before:scale-y-100 before:transition-transform hover:text-red-100 active:scale-y-100">
+                        <router-link :to="menu.route">{{ menu.title }}</router-link></li>
+
+                </ul>
+            </div>
+        </nav>
     </aside>
 </template>
 
 <script setup>
-import '../router';
+
+const menus = [
+    {
+        title: "Dashboard",
+        route: { path: '/dashboard' }
+    },
+    {
+        title: "Category Assets",
+        route: { path: '/category-assets' }
+    },
+]
+
+
 </script>
