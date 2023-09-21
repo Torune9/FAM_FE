cbcff72ed0504613e92e8f1890a2331051373266
rahman <rahmannurhayadi@gmail.com>
2023-09-21 14:41:27 +0700

    fix route master asset

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -10,9 +10,10 @@
                 </div>
                 <ul class="grid grid-rows-3 font-semibold text-sm">
                     <li v-for="menu, index in menus" :key="index"
-                        class="h-10 flex items-center p-2 before:contents[''] before:bg-red-300 before:h-10 before:w-1 before:mr-3 active:text-cyan-400 before:scale-y-0 hover:before:scale-y-100 before:transition-transform hover:text-red-100 active:scale-y-100">
-                        <router-link :to="menu.route">{{ menu.title }}</router-link></li>
-
+                        class="h-10 flex items-center p-2 before:contents[''] before:bg-red-300 before:h-10 before:w-1 before:mr-3 active:text-white before:scale-y-0 hover:before:scale-y-100 before:transition-transform hover:text-red-100 active:scale-y-100">
+                        
+                        <router-link :to="menu.route">{{ menu.title }}</router-link>
+                    </li>
                 </ul>
             </div>
         </nav>
@@ -20,8 +21,14 @@
 </template>
 
 <script setup>
+import { loginStore } from '../store/loginStore';
+import {ref,onMounted} from "vue"
 
-const menus = [
+
+const auth = loginStore()
+const menus = ref([])
+
+const links = [
     {
         title: "Dashboard",
         route: { path: '/dashboard' }
@@ -32,9 +39,16 @@ const menus = [
     },
     {
         title: "Master Data Asset",
-        route: {path : '/master-assets'}
+        route: {path : '/master-assets'},
+        name : 'master'
     },
 ]
-
+onMounted(()=>{
+    if (auth.user.role === "ADMIN") {
+       return menus.value = links
+    }else{
+        menus.value = links.filter(link => link.name != 'master' )
+    }
+})
 
 </script>
