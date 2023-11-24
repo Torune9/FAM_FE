41cd959435bddf1bc01388e7c976cc8355547a0e
rahman <rahmannurhayadi@gmail.com>
2023-11-24 09:54:08 +0700

    feat adding dropdown component

---
+++ src/components/dropdown/ContainerDropDown.vue
@@ -0,0 +1,26 @@
+<template>
+    <main class="relative">
+        <div class="flex justify-center items-center">
+            <button @click="isDrop" type="button"
+                class="inline bg-indigo-700 text-indigo-300 font-bold text-[10px] p-1 rounded-tl rounded-bl hover:bg-indigo-800 cursor-pointer">
+                    Manage
+            </button>
+            <span>
+                <button @click="isDrop" type="button"
+                    class="bg-indigo-600 w-6 h-[23px] rounded-tr rounded-br hover:bg-indigo-500 text-indigo-200 flex justify-center items-center">
+                    <font-awesome-icon icon="fa-solid fa-gear" />
+                </button>
+            </span>
+        </div>
+        <slot />
+    </main>
+</template>
+<script setup>
+import { ref, provide } from 'vue'
+
+const isActive = ref(false)
+const isDrop = () => {
+    isActive.value = !isActive.value
+}
+provide('isActive', isActive)
+</script>
---
+++ src/components/dropdown/ContentItem.vue
@@ -0,0 +1,47 @@
+<template>
+    <Transition name="item">
+        <main v-if="active" class="absolute z-10 right-[20%] top-6">
+            <ul class="bg-indigo-700 rounded overflow-hidden shadow-[3px_3px_rgba(0,0,0,0.7)]">
+                <li @click="data.popUp" class="border-b border-b-white p-1 text-indigo-200 text-[10px] font-barlow font-normal hover:bg-indigo-500 duration-500 transition-all cursor-pointer"
+                    v-for="data, i of menus" :key="i">
+                    {{ data.menu }}
+                </li>
+            </ul>
+        </main>
+    </Transition>
+</template>
+<script setup>
+import { inject,ref} from 'vue'
+const active = inject('isActive')
+const datas = ref([])
+const isShow = ref(false)
+const emits =defineEmits(['showModal'])
+const menus = [
+    {
+        menu : 'Change Role',
+        popUp : ()=>{
+            emits('showModal',isShow.value,datas.value)
+        },
+    },
+    {menu : 'Reset Password'},
+    {menu : 'Deactivate Account'}
+]
+</script>
+
+<style scoped>
+.item-enter-active {
+    transition: all 0.2s;
+}
+
+.item-leave-active {
+    transition: all 0.2s;
+}
+
+.item-enter-from {
+    transform: translateY(-5px);
+}
+
+.item-leave-to {
+    opacity: 0;
+    transform: translateY(-6px);
+}</style>
