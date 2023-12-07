0e7cb769cb45cc57fa462e168d1fe0230fc7c330
rahman <rahmannurhayadi@gmail.com>
2023-12-07 10:31:38 +0700

    fix(sysAdminStore) : fix updateUser

--- src/store/UserStore/sysAdmin.js
+++ src/store/UserStore/sysAdmin.js
@@ -3,13 +3,17 @@ import api from "../../service/api";
 
 export const sysAdminStore = defineStore('sysadmin',{
     actions:{
+        async updateUser(id,role){
+            return api.put(`/api/update/${id}`,role)
+            .then(res => res.data)
+          },
         async resetPassword(id,payload){
             return api.put(`api/users/reset-password/${id}`,payload)
             .then(res => res.data)
-            .catch(error => console.log(error))
         },
         async deactivateAccount(id,value){
             return api.get(`api/users/deactivate-account/${id}/${value}`)
+            .then(res => res.data)
         } 
     }
 })
