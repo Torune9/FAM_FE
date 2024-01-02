99490f631d60243d6ff1f698f20f643af8b2eb33
rahman <rahmannurhayadi@gmail.com>
2024-01-02 13:32:48 +0700

    fix  : delete attachment modal

--- src/views/history/HistoryView.vue
+++ src/views/history/HistoryView.vue
@@ -1,8 +1,6 @@
 <template>
     <MainLayout>
         <div class="flex flex-col">
-           <AttachmentModal :show-modal="showModal" 
-           @close="closeModal" :data="content" :show-attachment="attachment" ref="test"/>
             <h2 class="text-center text-3xl font-semibold text-slate-500">History Inspection</h2>
             <br>
             <div class="flex items-center">
@@ -13,32 +11,13 @@
             </div>
             <br>
             <EasyDataTable table-class-name="customizing-table" :headers="headers" :items="datas" :loading="loading" :rows-per-page=rows :rows-items=[rows] :fixed-expand="true">
+                <template #item-info ={information}>
+                    {{ information.substr(0,39)  }}...
+                </template>
                 <template #item-code="{ asset_code }">
                     <router-link :to="routes(asset_code)" class="text-blue-700 font-bold hover:underline hover:underline-offset-4 transition-all duration-500 hover:decoration-2 font-barlow ring-1 ring-blue-500 p-1 rounded">
                         {{ asset_code }}
                     </router-link>
-                </template>
-                <template #expand="item">
-                    <header>
-                        <h1 class="font-barlow text-blue-500">Information&nbsp;&#58;</h1>
-                        <p>{{ item.information }}</p>
-                    </header>
-                    <section>
-                        <h1 class="font-barlow text-blue-500">Image</h1>
-                        <details>
-                            <summary>file</summary>
-                            <a :href="link(item.file)" class="bg-blue-600 p-1 rounded text-white font-barlow font-light w-fit text-[10px]">
-                                {{ item.file }}
-                            </a>
-                        </details>
-                    </section>
-                </template>
-                <template #item-action="item">
-                    <div>
-                        <button @click="showInspect(item,true)" class="bg-yellow-400 font-semibold w-20 flex justify-center items-center rounded h-6 border-2 text-black/80 hover:bg-yellow-300 transition-all duration-200 hover:border-slate-400 p-3">
-                            <font-awesome-icon icon="fa-solid fa-square-plus" size="xl" />
-                        </button>
-                    </div>
                 </template>
             </EasyDataTable>
         </div>
@@ -50,17 +29,14 @@
     import {inspectStore} from '@/store/AssetStore/inspectStore'
     import { categoryStore } from '@/store/AssetStore/categoryStore';
     import {ref,onMounted,watch} from 'vue'
-    import AttachmentModal from '../../components/modal/assetManagementModal/AttachmentModal.vue';
-    
+
     const inspect = inspectStore()
     const category = categoryStore()
     const datas = ref([])
     const assets = ref([])
     const search = ref()
     const loading = ref(false)
-    const showModal = ref(false)
-    const content  = ref()
-    const attachment = ref(false)
+
     const rows = ref(8) 
     const url = import.meta.env.VITE_APP_BASE_URL
 
@@ -85,14 +61,14 @@
             width : 100
         },
         {
-            text :'Inspector',
-            value : 'inspector',
+            text :'Information',
+            value : 'info',
             width : 100
         },
         {
-            text :'Attachment',
-            value : 'action',
-            width : 200
+            text :'Inspector',
+            value : 'inspector',
+            width : 100
         },
     ]    
     const getHistory = ()=>{
@@ -103,6 +79,7 @@
         
         inspect.getHistory(payload)
         .then(res =>{
+            console.log(res.result.data);
             datas.value = res.result.data
         })
         .finally(()=>{
@@ -115,16 +92,6 @@
         .then(res=>{
             assets.value = res.result.content
         })
-    }
-
-    const showInspect = (item,show)=>{
-        content.value = item
-        showModal.value = true
-        attachment.value = show
-    }
-
-    const closeModal = ()=>{
-        showModal.value = false
     }
     
     watch(()=>search.value,()=>{
