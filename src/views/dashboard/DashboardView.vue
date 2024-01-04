c62d6bfbca06ed0fe509d2fa549f747d4e6f840c
rahman <rahmannurhayadi@gmail.com>
2024-01-04 10:08:31 +0700

    fix : fix username

--- src/views/dashboard/DashboardView.vue
+++ src/views/dashboard/DashboardView.vue
@@ -1,8 +1,8 @@
 <template>
     <MainLayout>
-      <div v-if="login.isLogged" class ="flex items-center justify-center h-screen text-[90px] font-bold text-slate-400 max-lg:text-6xl max-sm:text-4xl">
+      <div v-if="login.isLogged" class ="flex items-center justify-center h-screen text-[90px] font-bold text-slate-400 max-lg:text-6xl max-sm:text-4xl max-w-screen-xl">
         <h1>Welcome,</h1>
-        <span class="group" v-for="name of user" :key="name">
+        <span class="group" v-for="name of [...auth.getUsername]" :key="name">
           <h1 class="group-hover:scale-75 group-hover:font-black group-hover:bg-yellow-500 group-hover:text-black transition-all duration-300 group-hover:rounded group-hover:p-2 group-hover:animate-none ">{{ name }}
           </h1>
         </span>
@@ -13,24 +13,13 @@
 <script setup>
 
 import MainLayout from '@/layout/MainLayout.vue';
-
 import { loginStore } from "@/store/UserStore/loginStore"
-import {onBeforeMount,ref} from 'vue'
-import { useRouter } from "vue-router";
-const router = useRouter()
-const login = loginStore()
-const user = ref([])
-const {user:{username}} = login
-onBeforeMount(() => {
-  if (login.isLogged) {
-    user.value = [...username]
-  }else{
-    router.replace('/')
-  }
-})
+
+const auth = loginStore()
 
 </script>
-<style setup>
+
+<style>
  div{
   font-family: 'Poppins',sans-serif;
  }
