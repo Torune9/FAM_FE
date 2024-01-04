a12389cf91e129b01609f6ad48912ee18b911676
rahman <rahmannurhayadi@gmail.com>
2024-01-04 10:21:09 +0700

    fix : fix username

--- src/views/dashboard/DashboardView.vue
+++ src/views/dashboard/DashboardView.vue
@@ -1,6 +1,6 @@
 <template>
     <MainLayout>
-      <div v-if="login.isLogged" class ="flex items-center justify-center h-screen text-[90px] font-bold text-slate-400 max-lg:text-6xl max-sm:text-4xl max-w-screen-xl">
+      <div class ="flex items-center justify-center h-screen text-[90px] font-bold text-slate-400 max-lg:text-6xl max-sm:text-4xl max-w-screen-xl">
         <h1>Welcome,</h1>
         <span class="group" v-for="name of [...auth.getUsername]" :key="name">
           <h1 class="group-hover:scale-75 group-hover:font-black group-hover:bg-yellow-500 group-hover:text-black transition-all duration-300 group-hover:rounded group-hover:p-2 group-hover:animate-none ">{{ name }}
