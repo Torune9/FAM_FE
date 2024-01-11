2489075512a3772909129b14804c890513f57898
rahman <rahmannurhayadi@gmail.com>
2024-01-11 11:08:18 +0700

    style : change layout

--- src/components/modal/assetManagementModal/AssetModal.vue
+++ src/components/modal/assetManagementModal/AssetModal.vue
@@ -1,66 +1,70 @@
 <template>
     <Transition name="master-modal">
         <main v-if="modalPop" class="h-screen w-screen fixed z-10">
-            <div
-                class=" absolute bottom-24 bg-white w-72 h-96 flex flex-col justify-center items-center rounded border border-black">
-                <div class="mt-4" v-if="showAdd">
-                    <label for="name" class="font-bold font-barlow text-sm">Name</label>
-                    <br>
-                    <input v-model="payload.name" id="name" type="text"
-                        class="outline-none text-center border-2 border-slate-600 text-sm w-60 h-8 rounded-md font-semibold focus:border-2 text-black focus:border-blue-600">
-                </div>
-                <div v-if="showAdd">
-                    <label for="created" class="font-bold font-barlow text-sm">CreatedBy</label>
-                    <br>
-                    <input v-model="payload.created_by" id="created" type="text"
-                        class="outline-none text-center border-2 border-slate-600 text-sm w-60 h-8 rounded-md font-semibold focus:border-2 text-black focus:border-blue-600">
-                </div>
-                <div v-else>
-                    <label for="status" class="font-bold font-barlow text-sm">Status</label>
-                    <br>
-                    <input v-model="payload.status" id="status" type="text"
-                        class="outline-none text-center border-2 border-slate-600 text-sm w-60 h-8 rounded-md font-semibold focus:border-2 text-black focus:border-blue-600">
-                </div>
-                <div class="w-full flex justify-end flex-col p-2 mt-2">
-                    <label for="quantity" class="font-bold font-barlow text-sm text-end">Quantity</label>
-                    <div class="flex justify-end items-center gap-x-4 font-bold">
-                        <button @click="minQuantity" class="w-8 h-8 rounded bg-slate-300">-</button>
-                        <p class="w-10 text-center">{{ payload.quantity }}</p>
-                        <button @click="plusQuantity" class="w-8 h-8 rounded bg-slate-300">+</button>
+            <div class=" absolute bottom-24 min-w-[300px] bg-white flex flex-col rounded-md border border-black">
+                <!-- Modal Header -->
+                <section class="flex flex-row w-full border-b p-5">
+                    <h1 class="font-bold text-xl">{{ title }}</h1>
+                    <button @click="close(false)" class=" w-[40%] text-right text-black/40 hover:text-red-600 transition-all">
+                        <font-awesome-icon icon="fa-solid fa-rectangle-xmark" size="xl"/>
+                    </button>
+                </section>
+                <!-- Main Modal -->
+                <section class="p-5 flex flex-col gap-y-4">
+                    <div class="flex flex-col gap-y-1" v-if="showAdd">
+                        <label class="font-bold" for="name">Name</label>
+                        <input placeholder="Type asset name" v-model=payload.name class="border border-slate-500 rounded-md w-full h-10 text-sm pl-2 font-semibold outline-none hover:border-slate-800" type="text" id="name" name="name">
                     </div>
-                    <br>
-                    <input v-model="payload.quantity" id="quantity" type="number"
-                        class="hidden outline-none text-center border-2 border-slate-600 text-sm w-60 h-8 rounded-md font-semibold focus:border-2 text-black focus:border-blue-600">
-                </div>
-                <div v-if="showAdd">
-                    <select v-model="payload.code" name="category" id="category"
-                        class="w-full outline-none focus:border-slate-700 focus:border-2 h-8 text-sm rounded bg-slate-600 text-white border-slate-800 border-2">
-                        <option value="" disabled>Please select category</option>
-                        <option :value="category.category_code" v-for="category of category" :key="category">
-                            {{ category.category_name }}</option>
-                    </select>
-                </div>
+                    <div class="flex flex-col gap-y-1" v-else>
+                        <label class="font-bold" for="status">Status</label>
+                        <input placeholder="Type asset status" v-model=payload.status class="border border-slate-500 rounded-md w-full h-10 text-sm pl-2 font-semibold outline-none hover:border-slate-800" type="text" id="status" name="status">
+                    </div>
 
-                <div v-if="showAdd">
-                    <button @click="add"
-                        class="absolute bottom-2 right-2 bg-green-600 font-semibold w-32 h-10 rounded-md text-white text-sm border-2 hover:bg-green-500 transition-all">
-                        <p v-if="!loading">create</p>
-                        <font-awesome-icon icon="fa-solid fa-spinner" class="animate-spin text-white/40" size="xl" v-else />
-                    </button>
-                </div>
-                <div v-else>
-                    <button @click="update"
-                        class="absolute bottom-2 right-2 bg-blue-600 font-semibold w-32 h-10 rounded-md text-white text-sm border-2 hover:bg-blue-500 transition-all">
-                        <p v-if="!loading">update</p>
-                        <font-awesome-icon icon="fa-solid fa-spinner" class="animate-spin text-white/40" size="xl" v-else />
-                    </button>
-                </div>
-
-                <div>
-                    <button class="absolute top-2 left-2 hover:text-red-700 text-black/40" @click="close(false)">
-                        <font-awesome-icon icon="fa-solid fa-rectangle-xmark" size="xl" />
-                    </button>
-                </div>
+                    <div class="flex flex-row gap-x-3">
+                        <div v-if="showAdd" class="flex flex-col gap-y-1">
+                            <label for="createdBy" class="font-bold">createdBy</label>
+                            <input placeholder="Type owner" v-model="payload.created_by" type="text" name="createdBy" id="createdBy" class="border border-slate-500 w-full h-10 text-sm pl-2 font-semibold outline-none hover:border-slate-800 rounded-md">
+                        </div>
+                        <div class="flex flex-col gap-y-1">
+                            <label for="quanttiy" class="font-bold">Quantity</label>
+                            <input v-model="payload.quantity" type="text" name="quantitiy" id="quantity" class="hidden">
+                            <div class="flex flex-row justify-center items-center h-full rounded-md gap-2">
+                                <button @click="minQuantity" class="bg-slate-500 rounded-md w-10 h-full">
+                                    <font-awesome-icon icon="fa-solid fa-minus" />
+                                </button>
+                                <p class="w-5 text-center">{{ payload.quantity }}</p>
+                                <button @click="plusQuantity" class="bg-slate-500 rounded-md w-10 h-full min-h-[40px]">
+                                    <font-awesome-icon icon="fa-solid fa-plus" />
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                    <div v-if="showAdd" class="flex flex-col gap-y-1">
+                        <label class="font-bold" for="category">Category</label>
+                        <select v-model="payload.code" name="category" id="category"
+                            class="w-full outline-none focus:border-slate-700 focus:border-2 h-10 text-sm rounded-md bg-slate-500 text-white pl-2 cursor-pointer">
+                            <option value="" disabled>Please select category</option>
+                            <option :value="category.category_code" v-for="category of category" :key="category">
+                                {{ category.category_name }}</option>
+                        </select>
+                    </div>
+                </section>
+                <!-- footer modal -->
+                <section class="p-5">
+                   <div class="w-full text-right" v-if="showAdd">
+                        <button @click="add" class="bg-green-600 text-white h-10 w-28 rounded-md font-semibold hover:bg-green-500 transition-all duration-300">
+                                <p v-if="!loading">Create</p>
+                                <font-awesome-icon icon="fa-solid fa-spinner" class="animate-spin text-white/40" size="xl" v-else />
+                        </button>
+                        
+                   </div>
+                   <div class="w-full text-right" v-else>
+                        <button @click="update" class="bg-blue-600 text-white h-10 w-28 rounded-md font-semibold hover:bg-blue-500 transition-all duration-300">
+                                <p v-if="!loading">Update</p>
+                                <font-awesome-icon icon="fa-solid fa-spinner" class="animate-spin text-white/40" size="xl" v-else />
+                        </button>
+                   </div>
+                </section>
             </div>
         </main>
     </Transition>
@@ -93,6 +97,9 @@ const props = defineProps({
         default: () => {
             return {}
         }
+    },
+    title : {
+        type : String
     }
 })
 
@@ -167,12 +174,12 @@ const close = (needRefresh = false) => {
     emit("close", needRefresh)
 }
 
-const plusQuantity = ()=> payload.quantity++
+const plusQuantity = () => payload.quantity++
 
-const minQuantity = ()=> payload.quantity--
+const minQuantity = () => payload.quantity--
 
 watch(() => props.modalPop, handleShowUpdate)
-watchEffect(()=>{
+watchEffect(() => {
     if (payload.quantity < 0) {
         return payload.quantity = 0
     }
