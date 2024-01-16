ea0e151ac4065c6eea8426419b1f3dd3c54f92e0
rahman <rahmannurhayadi@gmail.com>
2024-01-16 10:31:09 +0700

    style : fix display

--- src/views/dashboard/DashboardView.vue
+++ src/views/dashboard/DashboardView.vue
@@ -1,11 +1,13 @@
 <template>
     <MainLayout>
-      <div class ="flex items-center justify-center h-screen text-[90px] font-bold text-slate-400 max-lg:text-6xl max-sm:text-4xl max-w-screen-xl">
+      <div class ="flex items-center justify-center w-full overflow-hidden h-screen text-[90px] font-bold text-slate-400 max-lg:text-6xl max-sm:text-4xl max-w-screen-xl flex-col">
         <h1>Welcome,</h1>
-        <span class="group" v-for="name of [...auth.getUsername]" :key="name">
-          <h1 class="group-hover:scale-75 group-hover:font-black group-hover:bg-yellow-500 group-hover:text-black transition-all duration-300 group-hover:rounded group-hover:p-2 group-hover:animate-none ">{{ name }}
-          </h1>
-        </span>
+        <div class="flex flex-row flex-wrap max-w-2x rounded bg-slate-100 p-2">
+          <span class="group" v-for="name of [...auth.getUsername]" :key="name">
+            <h1 class="group-hover:scale-75 group-hover:font-black group-hover:bg-yellow-500 group-hover:text-black transition-all duration-300 group-hover:rounded group-hover:p-2 group-hover:animate-none ">{{ name }}
+            </h1>
+          </span>
+        </div>
       </div>
     </MainLayout>
 </template>
